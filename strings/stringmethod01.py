d642a2ad714799a5fadb9b172242920579405eee
lcentauri <aaron.centauri@outlook.com>
2023-02-13 22:00:51 +0000

    Join and split

---
+++ strings/stringmethod01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# create a small string 
+lilstring ="Alta3 Reasearch offers classes on Python coding"
+newlist = lilstring.split(" ")
+print(newlist) 
+
+myipist = ["192", "168", "0", "12"]
+
+singleip = ".".join(myiplist)
+
+print(singleip)
