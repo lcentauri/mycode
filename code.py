f04ad837082b18f8b44f222814790c399f3ce957
lcentauri <aaron.centauri@outlook.com>
2023-02-14 18:02:23 +0000

    learning about dictonaries

---
+++ code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+wordbank= ["indentation", "spaces"] 
+
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+
+wordbank.append(4)
+
+num= input("Pick a student number!")
+
+num= int(input("Pick a student number!"))
+
+choice= int(input("Pick a student number!"))
+
+student_name= tlgstudents[choice]
+
+<student_name> always uses <4> <spaces> to indent.
+
